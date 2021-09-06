15b48c662678c336b46e5b9b5e4b9fe636a76d0b
UlisesFritzSerain <ulifritz8@gmail.com>
2021-09-06 00:32:33 -0300

    maquetando el footer

--- Sprints/Sprint2/app.js
+++ Sprints/Sprint2/app.js
@@ -1,8 +1,10 @@
-let express = require ('express');
-let app = express();
+const express = require ('express');
+const app = express();
 
-let path = require('path');
-app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"));
+const path = require('path');
+
 app.use(express.static('./public'));
 
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html'))) 
+
+app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"));
