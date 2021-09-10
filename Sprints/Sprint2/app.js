3001275e2a20d61fcebbaf5908ea26a6018e303d
UlisesFritzSerain <ulifritz8@gmail.com>
2021-09-10 15:05:27 -0300

    estilos del home y register

--- Sprints/Sprint2/app.js
+++ Sprints/Sprint2/app.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 app.use(express.static('./public'));
 
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html'))) 
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html'))) ;
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
 
 app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"));
