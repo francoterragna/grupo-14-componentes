cbd8a54cc44f22be5607f526ed8879d95013638b
francoterragna <francoterragna@hotmail.com>
2021-09-12 21:54:59 -0300

    detalle de producto

--- Sprints/Sprint2/app.js
+++ Sprints/Sprint2/app.js
@@ -7,8 +7,10 @@ app.use(express.static('./public'));
 
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html'))) ;
 
-app.get("/register", (req, res) =>{
+app.get('/register', (req, res) =>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 });
+
+app.get('/producto', (req,res) => res.sendFile(path.join(__dirname, '/views/producto.html')));
 
 app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"));
