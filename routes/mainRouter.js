087e54d3077b789757224f19c3f040b5c37f5b44
francoterragna <francoterragna@hotmail.com>
2021-10-24 20:43:05 -0300

    probando autoprefixer CSS

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -5,11 +5,11 @@ const mainController = require ('../controllers/mainController');
 router.get('/', mainController.index);
 
 router.get('/pruebaSession', (req,res) =>{
-    if(req.session.numeroDeVisitas == undefined){
-        req.session.numeroDeVisitas= 0;
+    if(req.session.visitas == undefined){
+        req.session.visitas= 0;
     }
-    req.session.numeroDeVisitas++;
-    res.send('Session tiene el número ' + req.session.numeroDeVisitas)
-})
+    req.session.visitas++;
+    res.send('Session tiene el número ' + req.session.visitas)
+});
 
 module.exports = router;
