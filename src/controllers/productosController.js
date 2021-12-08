45dd30f893c01bddccbe9ccc702876a4461a5b07
francoterragna <francoterragna@hotmail.com>
2021-12-08 20:30:31 -0300

    colocando fotos

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -11,18 +11,19 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const productosController = {
     detalle: (req,res) =>{
         let id= req.params.id;
-        // let product = products.find(product => product.id == id);
-        // res.render('detalleProducto', {product});
         db.Product.findByPk(id)
-        .then(product => res.render('detalleProducto', {product}))
+        .then((product) =>{
+            db.Image.findAll({
+                where: {product_id: id}
+            })
+            .then(img => res.render('detalleProducto', {product, img}))
+        })
         .catch(err => res.send(err))
     },
+
     carrito: (req,res) => res.render('carrito'),
 
      showProduct: (req,res) =>{
-        // let category = req.params.category;
-        // let productos = products.filter(producto => producto.category == category)
-        // res.render('productSelect', {productos})
         db.Product.findAll({
             where: {
                 category: req.params.category
@@ -34,10 +35,9 @@ const productosController = {
         .catch(err => res.send(err));
 
     },
+
     //HAY QUE APLICAR EL OFFSET PARA QUE TENGA VARIAS PÁGINAS
-    showDiscount: (req,res) => {  
-        // let enOferta = products.filter(producto=> producto.discount > 0);
-        // res.render('ofertas', {enOferta});
+    showDiscount: (req,res) => {
         let inicio = 0;
         db.Product.findAll({
             where: {
@@ -46,7 +46,10 @@ const productosController = {
             limit: 5,
             offset: inicio
         })
-        .then(enOferta => res.render('ofertas', {enOferta}))
+        .then((enOferta) =>{
+            db.Image.findAll()
+            .then((img) => res.render('ofertas', {enOferta, img}))
+        } )
         .catch(err => res.send(err))
     }
  }
