a87656bb97b990e9f1b4cad49bb83d4e6a61695a
francoterragna <francoterragna@hotmail.com>
2022-01-07 18:47:05 -0300

    validaciones register andan mal

--- public/js/frontRegister.js
+++ public/js/frontRegister.js
@@ -2,31 +2,32 @@
 
 window.addEventListener('load',function(){
 
+     let formulario = document.querySelector('form');
+     let campoNombre = document.querySelector('.input_name');
+     let campoApellido = document.querySelector('.input_lastName')
 
-    let formulario = document.querySelector('form')
-    formulario.addEventListener('submit', function(e){
-//  e.preventDefault();
 
-let errores = [];
+     let errores = [];
+     formulario.addEventListener('submit', function(e){
+        
+         if(campoNombre.value == '') {
+             errores.push('El campo de nombre tiene que estar completo');
+         }
+         if(campoNombre.value.length < 3) {
+             errores.push('El campo de nombre debe tener al menos 3 caracteres');
+         }
+         if(campoApellido.value == ''){
+             errores.push('El campo de apellido tiene que estar completo')
+         }
 
-let campoNombre = document.querySelector('.input_name');
-console.log(campoNombre.value);
-if(campoNombre.value =="") {
-    // alert('El campo tiene que estar completo')
-    errores.push('El campo tiene que estar completo');
-}
-else if(campoNombre.value.length < 3) {
-    // alert('El campo debe tener al menos 3 carácteres')
-    errores.push('El campo de nombre debe tener al menos 3 caracteres');
-}
-
-if(errores.length > 0) {
-e.preventDefault();
-
-let ulErrores = document.querySelector('.text-danger ul');
-for(let i = 0 ; i < errores.length; i++) {
-ulErrores.inner.HTML = '<li>' + errores[i] + '</li>'
-}
-}
-    })
+         let ulErrores = document.querySelector('.error-name');
+         if(errores.length > 0) {
+         e.preventDefault();
+         
+         alert('Hay errores en el formulario');
+         console.log(errores)
+                 errores.map(error => {ulErrores.innerHTML += '<li>' + error + '</li>'})
+            }
+            
+     })
 })
